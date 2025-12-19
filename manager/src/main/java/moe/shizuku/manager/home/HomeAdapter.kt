f4121cf3eec070f8f752744163ec2c86c4e187ff
Alex <achiommino@gmail.com>
2025-12-19 12:39:01 -0500

    add stealth mode card

--- manager/src/main/java/moe/shizuku/manager/home/HomeAdapter.kt
+++ manager/src/main/java/moe/shizuku/manager/home/HomeAdapter.kt
@@ -28,6 +28,7 @@ class HomeAdapter(private val homeModel: HomeViewModel, private val appsModel: A
         private const val ID_LEARN_MORE = 6L
         private const val ID_ADB_PERMISSION_LIMITED = 7L
         private const val ID_AUTOMATION = 8L
+        private const val ID_STEALTH = 9L
     }
 
     override fun onCreateCreatorPool(): IndexCreatorPool {
@@ -72,6 +73,7 @@ class HomeAdapter(private val homeModel: HomeViewModel, private val appsModel: A
             }
         }
         addItem(AutomationViewHolder.CREATOR, null, ID_AUTOMATION)
+        addItem(StealthViewHolder.CREATOR, null, ID_AUTOMATION)
         addItem(LearnMoreViewHolder.CREATOR, null, ID_LEARN_MORE)
         notifyDataSetChanged()
     }
