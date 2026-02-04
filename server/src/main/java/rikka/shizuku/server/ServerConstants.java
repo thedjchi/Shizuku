1742c4398aaab34c821579b5a010fa9965dea26f
Alex <achiommino@gmail.com>
2026-02-04 11:58:20 -0500

    keep static REQUEST_PERMISSION action but inject at build time

--- server/src/main/java/rikka/shizuku/server/ServerConstants.java
+++ server/src/main/java/rikka/shizuku/server/ServerConstants.java
@@ -1,10 +1,13 @@
 package rikka.shizuku.server;
 
+import moe.shizuku.server.BuildConfig;
+
 public class ServerConstants {
 
     public static final int MANAGER_APP_NOT_FOUND = 50;
 
     public static final String PERMISSION = "moe.shizuku.manager.permission.API_V23";
+    public static final String REQUEST_PERMISSION_ACTION = BuildConfig.MANAGER_APPLICATION_ID + ".intent.action.REQUEST_PERMISSION";
 
     public static final int BINDER_TRANSACTION_getApplications = 10001;
 }
