d7278e92bf367d3194af3c17b60addc3268903f1
Alex <achiommino@gmail.com>
2026-02-04 11:58:21 -0500

    implement stub apk feature

--- manager/src/main/java/moe/shizuku/manager/utils/ApkUtils.kt
+++ manager/src/main/java/moe/shizuku/manager/utils/ApkUtils.kt
@@ -1,12 +1,16 @@
 package moe.shizuku.manager.utils.ApkUtils
 
 import android.util.Log
+import com.reandroid.apk.AndroidFrameworks
 import com.reandroid.apk.ApkModule
+import com.reandroid.archive.ByteInputSource
+import com.reandroid.arsc.chunk.TableBlock
 import com.reandroid.arsc.chunk.xml.AndroidManifestBlock
 import com.reandroid.arsc.chunk.xml.ResXmlDocument
 import com.reandroid.arsc.chunk.xml.ResXmlElement
 import com.reandroid.common.Namespace
 import java.io.File
+import moe.shizuku.manager.R
 import moe.shizuku.manager.ShizukuApplication
 import moe.shizuku.manager.utils.ApkSigner
 
@@ -20,6 +24,18 @@ val workDir by lazy {
         it.deleteRecursively()
         it.mkdirs()
     }
+}
+
+fun ApkModule.buildAndSign(outFile: File): File {
+    Log.i(TAG, "Building new APK")
+    val unsignedApk = File(workDir, "unsigned.apk")
+    writeApk(unsignedApk)
+
+    Log.i(TAG, "Signing APK")
+    val key = ApkSigner.getOrCreateSigningKey(appContext.filesDir)
+    ApkSigner.sign(unsignedApk, outFile, key)
+
+    return outFile
 }
 
 fun File.changePackageName(newPkgName: String): File {
@@ -45,29 +61,56 @@ fun File.changePackageName(newPkgName: String): File {
         }
     }
 
-    Log.i(TAG, "Building new APK")
-    val unsignedApk = File(workDir, "unsigned.apk")
-    module.writeApk(unsignedApk)
-
-    Log.i(TAG, "Signing APK")
-    val signedApk = File(workDir, "signed.apk")
-    val key = ApkSigner.getOrCreateSigningKey(appContext.filesDir)
-    ApkSigner.sign(unsignedApk, signedApk, key)
-
-    return signedApk
+    val outFile = File(workDir, "signed.apk")
+    return module.buildAndSign(outFile)
 }
+
+fun createStubApk(pkgName: String): File {
+    val outFile = File(appContext.filesDir, "stub.apk")
+
+    val tableBlock = TableBlock()
+    val manifest = AndroidManifestBlock()
+    val dummyDex = ByteInputSource(ByteArray(0), "classes.dex")
+
+    val module = ApkModule().apply {
+        setTableBlock(tableBlock)
+        setManifest(manifest)
+        add(dummyDex)
+    }
+
+    val packageBlock = tableBlock.newPackage(0x7f, pkgName)
+    val appName = packageBlock.getOrCreate("", "string", "app_name").apply {
+        setValueAsString("${getAppLabel()} Stub")
+    }
+    val appIcon = packageBlock.getOrCreate("", "drawable", "ic_launcher").apply {
+        setValueAsReference(R.drawable.ic_launcher)
+    }
+
+    manifest.apply {
+        setPackageName(pkgName)
+        setVersionCode(1)
+        setVersionName("1.0.0")
+        setApplicationLabel(appName.getResourceId())
+        setIconResourceId(appIcon.getResourceId())
+        setTargetSdkVersion(app.applicationInfo.targetSdkVersion)
+        setMinSdkVersion(app.applicationInfo.minSdkVersion)
+    }
+
+    return module.buildAndSign(outFile)
+}
+
+private fun getAppLabel(): String = app
+    .applicationInfo
+    .loadLabel(appContext.packageManager)
+    .toString()
 
 fun buildApkFilename(): String {
     val pm = appContext.packageManager
-    val label = app
-        .applicationInfo
-        .loadLabel(pm)
-        .toString()
     val versionName = pm
         .getPackageInfo(app.packageName, 0)
         .versionName
 
-    val safeLabel = label
+    val safeLabel = getAppLabel()
         .lowercase()
         .replace("[^a-z0-9._-]".toRegex(), "-")
 
